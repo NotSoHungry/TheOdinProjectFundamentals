74148d6e5fedd40a2c49058236ef2fabd92c53b5
Pawel Bielinski <pawbielinski@gmail.com>
2018-04-25 17:45:15 +0200

    finish some exercises

--- reverseString/reverseString.js
+++ reverseString/reverseString.js
@@ -1,5 +1,11 @@
-var reverseString = function() {
-
+var reverseString = function(str) {
+    let newStr = "";
+    
+    for (let i = str.length - 1; i >= 0; i--) {
+      newStr += str[i];
+    }
+    
+    return newStr;
 }
 
 module.exports = reverseString
--- tempConversion/tempConversion.js
+++ tempConversion/tempConversion.js
@@ -1,9 +1,21 @@
-var ftoc = function() {
-  
+var ftoc = function(input) {
+  let convertedValue = (input - 32) / 1.8;
+
+  if (Number.isInteger(convertedValue)) {
+    return convertedValue;
+  } else {
+    return Number(convertedValue.toFixed(1));
+  }
 }
 
-var ctof = function() {
-  
+var ctof = function(input) {
+  let convertedValue = (input * 1.8) + 32;
+
+  if (Number.isInteger(convertedValue)) {
+    return convertedValue;
+  } else {
+    return Number(convertedValue.toFixed(1));
+  }
 }
 
 module.exports = {
